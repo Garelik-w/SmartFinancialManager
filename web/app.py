3c44ffe2cc311afe971be809dbd88fa54a56296d
GarryLin <515337036@qq.com>
2019-10-24 16:44:54 +0800

    implement a base web framework with flask and jinja2 MVC

---
+++ web/app.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# ======================
+# author:GarryLin-w
+# time: 2019/10/24:15:26
+# email:515337036@qq.com
+# =====================
+
+from flask  import Flask, request, render_template
+
+app = Flask(__name__)
+
+@app.route('/', methods=['GET','POST'])
+def home():
+    return render_template('home.html')
+#    return '<h1>Hello GarryLin!</h1>'
+
+@app.route('/signin', methods=['GET'])
+def signin_form():
+    return render_template('form.html')
+
+@app.route('/signin', methods=['POST'])
+def signin():
+    # 需要从request对象读取表单内容：
+    username = request.form['username']
+    password = request.form['password']
+    if username == 'admin' and password == 'password':
+        return render_template('signin-ok.html', username=username)
+    return render_template('form.html', message='Bad username or password', username=username)
+
+if __name__ == '__main__':
+    app.run()
