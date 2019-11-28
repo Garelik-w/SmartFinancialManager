cb3254918b642a76b6f3c0dc37d89ac225773ea4
GarryLin <515337036@qq.com>
2019-11-28 17:50:08 +0800

    change some bug for deloy v1.0

--- app/auth/views.py
+++ app/auth/views.py
@@ -6,7 +6,7 @@
 # email:515337036@qq.com
 # ======================
 
-from flask import render_template,redirect, request, url_for, flash
+from flask import render_template, redirect, request, url_for, flash
 from flask_login import login_user, logout_user, login_required, current_user
 from . import auth
 from .. import db
--- config.py
+++ config.py
@@ -1,5 +1,13 @@
 import os
-from private import Private
+try:
+    from private import Private
+except ImportError:
+    class Private(object):
+        MAIL_USERNAME = ''
+        MAIL_PASSWORD = ''
+        SQLALCHEMY_DEVELOP_DATABASE_URI = ""
+        SQLALCHEMY_TEST_DATABASE_URI = ""
+
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 
@@ -12,7 +20,6 @@ class Config:
     MAIL_USE_TLS = True
     # 配置批处理邮件设置
     FLASK_MAIL_SUBJECT_PREFIX = '[SmartFinancialManager]'  # 主题前缀
-    FLASK_MAIL_SENDER = 'SmartFinancialManager Admin <515337036@qq.com>'  # 发送者信息
     FLASK_ADMIN = 'zhuiyiyydyy@163.com'  # 管理员地址
     # 配置数据库信息
     SQLALCHEMY_TRACK_MODIFICATIONS = False
@@ -35,6 +42,7 @@ class DevelopmentConfig(Config):
     # 配置邮箱用户信息
     MAIL_USERNAME = Private.MAIL_USERNAME
     MAIL_PASSWORD = Private.MAIL_PASSWORD
+    FLASK_MAIL_SENDER = 'SmartFinancialManager Admin <515337036@qq.com>'  # 发送者信息
     # 配置数据库信息
     SQLALCHEMY_DATABASE_URI = Private.SQLALCHEMY_DEVELOP_DATABASE_URI
 
@@ -42,41 +50,46 @@ class DevelopmentConfig(Config):
 class TestingConfig(Config):
     TESTING = True
     WTF_CSRF_ENABLED = False  # 禁用flask-WTF的CSRF
+    FLASK_MAIL_SENDER = 'SmartFinancialManager Admin <515337036@qq.com>'  # 发送者信息
     # 配置数据库信息
     SQLALCHEMY_DATABASE_URI = Private.SQLALCHEMY_TEST_DATABASE_URI
 
 
 class ProductionConfig(Config):
     # 配置数据库信息
-    SQLALCHEMY_DATABASE_URI = Private.SQLALCHEMY_PRODUCTION_DATABASE_URI
+    try:
+        SQLALCHEMY_DATABASE_URI = Private.SQLALCHEMY_PRODUCTION_DATABASE_URI
+    except:
+        SQLALCHEMY_DATABASE_URI = "mysql://root:w123456@49.235.42.145:3306/production-db"
     # 配置邮箱用户信息
-    MAIL_USERNAME = Private.MAIL_USERNAME
-    MAIL_PASSWORD = Private.MAIL_PASSWORD
+    MAIL_USERNAME = '515337036@qq.com'  # 这里我需要修改
+    MAIL_PASSWORD = 'xfiuzbwzliicbjcf'
+    FLASK_MAIL_SENDER = 'SmartFinancialManager Admin <515337036@qq.com>'  # 发送者信息
 
     # 部署-日志记录器
-    @classmethod
-    def init_app(cls, app):
-        Config.init_app(app)
-
-        # email errors to the administrators
-        import logging
-        from logging.handlers import SMTPHandler
-        credentials = None
-        secure = None
-        if getattr(cls, 'MAIL_USERNAME', None) is not None:
-            credentials = (cls.MAIL_USERNAME, cls.MAIL_PASSWORD)
-            if getattr(cls, 'MAIL_USE_TLS', None):
-                secure = ()
-        # 基于SMTP简单邮件协议服务器发送邮件
-        mail_handler = SMTPHandler(
-            mailhost=(cls.MAIL_SERVER, cls.MAIL_PORT),
-            fromaddr=cls.FLASK_MAIL_SENDER,
-            toaddrs=[cls.FLASK_ADMIN],
-            subject=cls.FLASK_MAIL_SUBJECT_PREFIX + ' Application Error',
-            credentials=credentials,
-            secure=secure)
-        mail_handler.setLevel(logging.ERROR)
-        app.logger.addHandler(mail_handler)
+    # @classmethod
+    # def init_app(cls, app):
+    #     Config.init_app(app)
+    #
+    #     # email errors to the administrators
+    #     import logging
+    #     from logging.handlers import SMTPHandler
+    #     credentials = None
+    #     secure = None
+    #     if getattr(cls, 'MAIL_USERNAME', None) is not None:
+    #         credentials = (cls.MAIL_USERNAME, cls.MAIL_PASSWORD)
+    #         if getattr(cls, 'MAIL_USE_TLS', None):
+    #             secure = ()
+    #     # 基于SMTP简单邮件协议服务器发送邮件
+    #     mail_handler = SMTPHandler(
+    #         mailhost=(cls.MAIL_SERVER, cls.MAIL_PORT),
+    #         fromaddr=cls.FLASK_MAIL_SENDER,
+    #         toaddrs=[cls.FLASK_ADMIN],
+    #         subject=cls.FLASK_MAIL_SUBJECT_PREFIX + ' Application Error',
+    #         credentials=credentials,
+    #         secure=secure)
+    #     mail_handler.setLevel(logging.ERROR)
+    #     app.logger.addHandler(mail_handler)
 
 
 # 在程序工厂函数根据需要导入对应的配置
